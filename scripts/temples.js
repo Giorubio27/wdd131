f90269fe60283ca6a69327be7a12e036a20e8791
Giorubio27 <Giorubio27@gmail.com>
2025-11-15 16:20:48 -0700

    hey

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,2 +1,12 @@
 
 document.getElementById("last_modified").innerHTML = document.lastModified;
+
+
+const navigation = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+
+hambutton.addEventListener('click', () => {
+    navigation.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
