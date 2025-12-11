3b7451b49c9edb58eee613ce1dcc5a047ce82ac4
Giorubio27 <Giorubio27@gmail.com>
2025-12-11 08:56:59 -0700

    hoopla

--- project/scripts/site.js
+++ project/scripts/site.js
@@ -1,2 +1,4 @@
 document.getElementById("last_modified").innerHTML = document.lastModified;
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+
+const hambutton = document.querySelector('#menu');
