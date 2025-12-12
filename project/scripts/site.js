496487e9167ea0dfb12d2cc77db813b61be47bbd
Giorubio27 <Giorubio27@gmail.com>
2025-12-12 12:04:36 -0700

    help

--- project/scripts/site.js
+++ project/scripts/site.js
@@ -2,3 +2,137 @@ document.getElementById("last_modified").innerHTML = document.lastModified;
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 
 const hambutton = document.querySelector('#menu');
+
+const teams = [
+    {
+        id: "sas",
+        name: "Spurs"
+    },
+    {
+        id: "houston",
+        name: "Rockets"
+       
+    },
+    {
+        id: "nyk",
+        name: "Knicks"
+        
+    },
+    {
+        id: "boston",
+        name: "Celtics"
+        
+    },
+    {
+        id: "indy",
+        name: "Pacers"
+        
+    }
+];
+
+
+teamDropDown = function teamDropDown() {
+    const teamSelect = document.getElementById("team-select");
+
+    teams.forEach(product => {
+        const option = document.createElement("option");
+        option.value = product.id;
+        option.textContent = product.name;
+
+        teamSelect.appendChild(option);
+
+    });
+}
+document.addEventListener('DOMContentLoaded', teamDropDown);
+// const
+//     visitDisplay = document.querySelector(".visit");
+
+// let numVisits = Number(window.localStorage.getItem("numVisits - ls")) || 0;
+
+
+
+// numVisits++;
+
+// if (numVisits !== 0) {
+//     visitDisplay.textContent = `Number of Visits:  ${numVisits}`;
+// } else {
+//     visitDisplay.textContent = `Welcome to our site!`;
+// }
+
+// window.localStorage.setItem("numVisits - ls", numVisits);
+
+// network
+
+const networks = [
+    {
+        id: "espn",
+        name: "ESPN"
+    },
+    {
+        id: "abc",
+        name: "ABC"
+
+    },
+    {
+        id: "fox",
+        name: "FOX Sports"
+
+    },
+    {
+        id: "tnt",
+        name: "TNT"
+
+    },
+    {
+        id: "nba",
+        name: "NBA League Pass"
+
+    }
+];
+
+
+networkDropDown = function networkDropDown() {
+    const networkSelect = document.getElementById("network-select");
+
+    networks.forEach(network => {
+        const option = document.createElement("option");
+        option.value = network.id;
+        option.textContent = network.name;
+
+        networkSelect.appendChild(option);
+
+    });
+}
+document.addEventListener('DOMContentLoaded', networkDropDown);
+
+
+const seasons = [
+    {
+        id: "preseason",
+        name: "Preseason"
+    },
+
+    {
+        id: "reg",
+        name: "Regular Season"
+    },
+    {
+        id: "post",
+        name: "Post-Season"
+    }
+];
+
+seasonDropDown = function seasonDropDown() {
+    const seasonSelect = document.getElementById("season-select");
+
+    seasons.forEach(season => {
+        const option = document.createElement("option");
+        option.value = season.id;
+        option.textContent = season.name;
+
+        seasonSelect.appendChild(option)
+    });
+}
+document.addEventListener('DOMContentLoaded', seasonDropDown);
+
+
